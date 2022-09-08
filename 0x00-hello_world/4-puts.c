f22b6dd6789da9dda7c6487692b3e64fddad9fd6
sulemame1 <sulemame11@gmail.com>
2022-09-08 08:03:01 -0700

    4 puts c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h
+/**
+ * *Description: main prints out a string to stdout
+ * *Return: 0 if sucess
+ */
+int main (void)
+{
+	puts("\"programming is like a building a multilingual puzzle");
+	return 0;
+}
