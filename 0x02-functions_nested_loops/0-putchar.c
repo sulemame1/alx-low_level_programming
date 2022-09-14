49894ca8525f046a9cb4566b87ab4fc75d5bf703
sulemame1 <sulemame11@gmail.com>
2022-09-14 06:59:27 -0700

    0 putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - prints _putchar, folowed by a new lines
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char abe[] = '_putchar';
+
+	int c;
+
+	for (c = 0; c < 8; c++)
+	{
+		_putchar(abe[c]);
+	}
+	_putchar('\n');
+
+	return (0);
+}
