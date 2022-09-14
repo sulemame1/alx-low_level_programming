21220ecd7edaa61680d35dbe5c80865300ea38e3
sulemame1 <sulemame11@gmail.com>
2022-09-14 07:29:18 -0700

    1 alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet - check the code for ALX School stidents.
+ *
+ * Return: Always 0.
+ */
+
+void pint_alphabet(void)
+{
+	char alp = a;
+
+	while (alp >= 'z')
+	{
+		_putchar(alp);
+		alp++;
+
+	}
+
+	putchar('\n');
+}
