b958e78d1acbb16bcc116947c0429cce6a21f926
sulemame1 <sulemame11@gmail.com>
2022-09-22 06:52:59 -0700

    6 cap string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,75 @@
+#include "main.h"
+
+
+
+/**
+ *
+ *  * string_toupper -  function that changes all lowercase letter
+ *
+ *   * of a string to uppercase.
+ *
+ *    *@n: cadena de caracteres.
+ *
+ *     * Return: n.
+ *
+ *      */
+
+char *cap_string(char *s)
+
+{
+
+		int i;
+
+
+
+			i = 0;
+
+				while (s[i] != '\0')
+
+						{
+
+									if ((s[i - 1] == ' ' || s[i - 1] == '\n'
+
+														|| s[i - 1] == '\t' || s[i - 1] == ','
+
+																|| s[i - 1] == ';' || s[i - 1] == '!'
+
+																		|| s[i - 1] == '?' || s[i - 1] == '"'
+
+																				|| s[i - 1] == '(' || s[i - 1] == ')'
+
+																						|| s[i - 1] == '{' || s[i - 1] == '}'
+
+																								|| s[i - 1] == '.')
+
+													&& (s[i] >= 'a' && s[i] <= 'z'))
+
+												{
+
+																s[i] = s[i] - 32;
+
+																		}
+
+											else if ((s[0] >= 97 && s[0] <= 122))
+
+														{
+
+																		s[0] = s[0] - 32;
+
+																				}
+
+													else
+
+																{
+
+																				s[i] = s[i];
+
+																						}
+
+															i++;
+
+																}
+
+					return (s);
+
+}
